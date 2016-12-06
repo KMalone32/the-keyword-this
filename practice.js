4e32aa6604a7c7b5d8d28f0f3cb0f4f5049858c9
William Cox <willubyu7@gmail.com>
2016-12-06 13:32:28 -0700

    finished the-keyword-this challenges

--- practice.js
+++ practice.js
@@ -1,7 +1,7 @@
 //We're in a job interview. Answer the following questions (try to not look at your notes unless you have to).
   // 1) What is the purpose of the 'this keyword'?
 
-      //Answer: To represent and reference the context of the invoked function.
+      //Answer: To represent and reference the context of the invoked function. 
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
